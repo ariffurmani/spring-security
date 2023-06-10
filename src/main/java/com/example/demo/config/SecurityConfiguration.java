fba62e48c5f55ca5c5a872d97a81b293d7dfc24f
ariffurmani <arifforcoding@gmail.com>
2023-06-10 14:22:40 +0530

    Add another user credential to the same previous one

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -12,11 +12,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.inMemoryAuthentication().withUser("admin").password("admin12345").roles("USER");
+		authenticationManagerBuilder.inMemoryAuthentication()
+							.withUser("admin")
+							.password("admin12345")
+							.roles("ADMIN")
+							.and()
+							.withUser("user")
+							.password("user12345")
+							.roles("USER");
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
+		// This is just for test purpose and not recommended
 		return NoOpPasswordEncoder.getInstance();
 	}
 }
